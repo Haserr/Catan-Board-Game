f462f9652340bc2a3048c53d47491135b225c5de
Haserr <Haserr17@gmail.com>
2024-05-28 14:58:54 -0700

    Completely fixed the collision. When I update the house class to shift the house so it is centered, the coordinates were not being updated correctly. They are now all being updated and stored in the house class. Also, in the function "collideRectPoly", I was failing to check one of the line segments in the hexagon. This is now being checked and collision works as it should.

--- Catan/BoardSetup.py
+++ Catan/BoardSetup.py
@@ -68,7 +68,11 @@ class Board():
     # Function to draw a hexagon
     def draw_hexagon(self,center, size):
         points = self.hexagon_points(center, size)
+        
+        # Draws a black hexagon to act as a black border
         pygame.draw.polygon(window, blackColor, points,3)
+        
+        # Draws the filled in hexagon
         pygame.draw.polygon(window, random.choice(tileColorList), points)
    
 
@@ -98,10 +102,11 @@ class Board():
     def hexLeft(self,center,size):
         newCenter = (center[0] - 1.73 * size, center[1])
         return newCenter  
-
+    
+    # Draws the whole board using the hex functions
     def boardSetup(self,center, size):
     
-        # ---- Start of Draing ----
+        # ---- Start of Drawing ----
         self.draw_hexagon(center,size)
         temp = center
     
@@ -184,10 +189,19 @@ def collideRectLine(rect, p1, p2):
 # Detects if a polygon and a rectangel are intersection
 # Achieved by testing each line segment in a polygone against the rectangle
 def collideRectPoly(rect, poly):
+    #print(poly[0], poly[1])
     #print(poly[1], poly[2])
+    #print(poly[2], poly[3])
+    #print(poly[3], poly[4])
+    #print(poly[4], poly[5])
+    #print(poly[5], poly[0])
     #print(rect) 
     for i in range(len(poly) - 1):
          if collideRectLine(rect, poly[i], poly[i+1]):
+            print(" --------- Collision!!! ------------")
+            return True
+         # Added this since line segment of the hexagon never gets checked 
+         if collideRectLine(rect, poly[5], poly[0]):
             print(" --------- Collision!!! ------------")
             return True
     return False
@@ -222,15 +236,24 @@ class House():
         
     # Setting the new position of a rectangle to mouse position
     def setXY(self,xy):
-        self.x1,self.y1 = xy
-        self.x2 = xy[0] + self.w
-        self.y2 = xy[1] + self.h
+        # The following 2 lines update the rectangle coords 
+        # Now it will be placed 0.5 * width and 0.5 * height 
+        # This centers the house on the mouse, so where ever you click is more accurate
         
-        # Sets references for collision detection
-        self.topLeft = [xy[0],xy[1]]
-        self.topRight = [xy[0]+self.w,xy[1]]
-        self.bottomLeft = [xy[0],xy[1]+self.h]
-        self.bottomRight = [xy[0]+self.w,xy[1]+self.h]
+        # Sets position of x1 and y1 and shifts rectangle to middle of mouse
+        self.x1 = xy[0] - (0.5*self.w)
+        self.y1 = xy[1] - (0.5*self.h)
+        
+        
+        # Sets x2 and y2 based off of the shifted position
+        self.x2 = self.x1 + self.w
+        self.y2 = self.y1 + self.h
+        
+        # Sets references for collision detection, not used for drawing
+        self.topLeft = [self.x1,self.y1]
+        self.topRight = [self.x1+self.w,self.y1]
+        self.bottomLeft = [self.x1,self.y1+self.h]
+        self.bottomRight = [self.x1+self.w,self.y1+self.h]
         
     # Returns a coordinate
     def getXY(self):
@@ -238,12 +261,11 @@ class House():
     
     # Returns values needed to build a rectangle in the form of a tuple
     def rect(self):
-        # The following 3 lines update the rectangle coords 
-        # Now it will be placed 0.5 * width and 0.5 * height 
-        # This centers the house on the mouse, so where ever you click is more accurate
+        
         coords = self.getXY()
-        x1 = coords[0] - (0.5*self.w)
-        y1 = coords[1] - (0.5*self.h)
+        
+        x1 = coords[0] 
+        y1 = coords[1]
         
         return (x1,y1,self.w, self.h)
     
